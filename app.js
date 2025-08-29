5933725dfd225b5bc97b44f5342c4a3dd45eda78
Julio Villanueva <julioavr87@gamil.com>
2025-08-29 13:17:34 -0600

    Se agrego el sistema para ingresar nombres para ser soerteados y que el usuario los vea

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 
 let listadeamigos = []
 
-
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
 
 function agregarAmigo() {
     let nombreamigo = document.getElementById('amigo').value;
-    console.log(nombreamigo);
     if (nombreamigo === "") {
         alert('debes de ingresar un nombre valido')
+        return
     } else {
         if (listadeamigos.includes(nombreamigo)) {
         alert('nombre ya agregado');
+        return
         } else {
         listadeamigos.push(nombreamigo);
-        return nombreamigo;
+        mostrarNombres();
+        document.getElementById('amigo').value = "";
         }
     }
+}
+
+function mostrarNombres() {
+  let nombre = "";
+  for (let i = 0; i < listadeamigos.length; i++) {
+    nombre += "<li>" + listadeamigos[i] + "</li>";
+  }
+  asignarTextoElemento('#listaAmigos', nombre);
+}
+
+
+function condicionesIniciales() {
+    asignarTextoElemento('#listaAmigos',"");
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
 }
